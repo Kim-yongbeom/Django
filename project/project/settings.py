ed2b13fe7311f2137b1d1a5e690282fabc02fa44
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-01-12 00:36:00 +0900

    navbar

--- project/project/settings.py
+++ project/project/settings.py
@@ -124,4 +124,8 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
