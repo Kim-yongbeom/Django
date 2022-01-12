bf1531c625fa2d08551cfc192176eaa45bab0022
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-01-13 00:21:46 +0900

    project

--- project/mainPage/views.py
+++ project/mainPage/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     print('mainPage 호출됨')
-    mbti_list = {'mbti': ['ENFJ','ENFP','ENTJ','ENTP','ESFJ',
+    context = {'mbti': ['ENFJ','ENFP','ENTJ','ENTP','ESFJ',
                  'ESFP','ESTJ','ESTP','INFJ','INFP',
-                 'INTJ','INTP','ISFJ','ISFP','ISTJ','ISTP']}
-    return render(request, 'mainPage/main.html',mbti_list)
+                 'INTJ','INTP','ISFJ','ISFP','ISTJ','ISTP'],
+               }
+    return render(request, 'mainPage/main.html',context)
