755e4c078dad0cf33a28ed9f28758a26a96d0ff3
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-01-14 17:45:30 +0900

    project

--- project/mainPage/models.py
+++ project/mainPage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Good(models.Model):
+    imgs = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    mood1 = models.CharField(max_length=200)
+    mood2 = models.CharField(max_length=200)
+    mood3 = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
--- project/mainPage/views.py
+++ project/mainPage/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from mainPage.models import Good
+
+
 def main(request):
     print('mainPage 호출됨')
+    goods_list = Good.objects.all()
     context = {'mbti': ['ENFJ','ENFP','ENTJ','ENTP','ESFJ',
                  'ESFP','ESTJ','ESTP','INFJ','INFP',
                  'INTJ','INTP','ISFJ','ISFP','ISTJ','ISTP'],
+               'goods' : goods_list
                }
     return render(request, 'mainPage/main.html',context)
